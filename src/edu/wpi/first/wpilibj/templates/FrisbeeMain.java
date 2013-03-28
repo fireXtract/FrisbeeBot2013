3992ffbd86e742c012325d9737a3727b0e7ace20
Lucas Jed <yo6luk@yahoo.com>
2013-03-28 16:56:33 -0500

    Slimmed down code from UoM

--- src/edu/wpi/first/wpilibj/templates/FrisbeeMain.java
+++ src/edu/wpi/first/wpilibj/templates/FrisbeeMain.java
@@ -6,10 +6,7 @@
 /*----------------------------------------------------------------------------*/
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -21,27 +18,43 @@ import edu.wpi.first.wpilibj.Timer;
  * directory.
  */
 public class FrisbeeMain extends SimpleRobot {
-//    public Compressor airCompressor = null; 
-
+    
+    public FrisbeeMain() {
+        FrisbeeParts.getInstance().airComp.start();
+    }
+    
+    protected void disabled() {
+        getWatchdog().setEnabled(false);
+    }
+    
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
-    public void robotInit() {
-        FrisbeeParts.getInstance();
-    }
 
-    protected void disabled() {
-        stop();
-        getWatchdog().setEnabled(false);
-    }
-
-    public void stop() {
-        System.out.println("I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.");
-        FrisbeeParts setOfParts = FrisbeeParts.getInstance();
-//        setOfParts.airComp.stop();
-    }
 
     public void autonomous() {
+        while (isAutonomous() && isEnabled()) {
+            FrisbeeParts s = FrisbeeParts.getInstance();
+            s.cD1.set(false);
+            s.cD2.set(false);
+            s.cU1.set(false);
+            s.cU2.set(false);
+            s.Dropper.set(.5);
+            Timer.delay(1);
+            s.Dropper.set(0);
+            s.ShootFront.set(1);
+            s.ShootBack.set(.8);
+            Timer.delay(.8);
+            s.lU.set(true);
+            Timer.delay(0.2);
+            s.lU.set(false);
+            s.lD.set(true);
+            Timer.delay(0.1);
+            s.lD.set(false);
+            s.ShootFront.set(0);
+            s.ShootBack.set(0);
+        }
+
     }
 
     public void runRFDrive(FrisbeeParts q) {
@@ -61,53 +74,42 @@ public class FrisbeeMain extends SimpleRobot {
     }
 
     public void runShooter1(FrisbeeParts q) {
-        q.Shoot1.set(q.joy1.getRawButton(1) ? -1 : 0);
+        q.ShootFront.set(q.joy1.getRawButton(1) ? 1 : 0);
     }
 
     public void runShooter2(FrisbeeParts q) {
-        q.Shoot2.set(q.joy1.getRawButton(1) ? -0.8 : 0);
+        q.ShootBack.set(q.joy1.getRawButton(1) ? 0.8 : 0);
     }
 
     public void runDropper(FrisbeeParts q) {
-        q.Dropper.set(q.joy1.getRawButton(3) ? 0.5 : 0);
+        q.Dropper.set(q.joy1.getRawButton(2) ? 0.5 : 0);
     }
 
     public void runLauncher(FrisbeeParts q) {
-        q.sU.set(false);
-        q.sD.set(false);
-        if (q.joy1.getRawButton(4)) {
-            System.out.println("WORKSOFARb3");
-            q.sU.set(true);
-            Timer.delay(0.2);
-            q.sU.set(false);
-            q.sD.set(true);
-            Timer.delay(0.1);
-            q.sD.set(false);
-        }
-    }
-
-    public boolean checkStartComp(FrisbeeParts q) {
-        if (q.IOComp = true) {  //Check your pressure here.  If it is too low, return true, else return false.  True will eventually turn on the comp and false will turn it off.
-            q.airComp.start();
-            return true;
+        if (q.joy1.getRawButton(3)) {
+            q.lU.set(true);
+            q.lD.set(false);
         } else {
-            q.airComp.stop();
-            return false;
+            q.lU.set(false);
+            q.lD.set(true);
         }
     }
 
-    public void OtoggleComp(FrisbeeParts q) {
-        if (q.joy1.getRawButton(10) == true) {
-            q.IOComp = false;
-            System.out.println(q.IOComp);
-
+    public void climbMechU(FrisbeeParts q) {
+        if (q.joy1.getRawButton(4)) {
+            q.cD1.set(false);
+            q.cD2.set(false);
+            q.cU1.set(true);
+            q.cU2.set(true);
         }
     }
 
-    public void ItoggleComp(FrisbeeParts q) {
-        if (q.joy1.getRawButton(11) == true) {
-            q.IOComp = true;
-            System.out.println(q.IOComp);
+    public void climbMechD(FrisbeeParts q) {
+        if (q.joy1.getRawButton(5)) {
+            q.cU1.set(false);
+            q.cU2.set(false);
+            q.cD1.set(true);
+            q.cD2.set(true);
         }
     }
 
@@ -117,38 +119,23 @@ public class FrisbeeMain extends SimpleRobot {
     public void operatorControl() {
         System.out.println("Affirmative, Dave. I read you.");
         while (isOperatorControl() && isEnabled()) {
-
             FrisbeeParts setOfParts = FrisbeeParts.getInstance();
 
-            /*
-             * This is how you declare a compressor.  It is not part of the
-             * FrisbeeParts option.
-             * You need to specify the correct pressureSwitchChannel and
-             * compressorRelayChannel.  I am not sure what these are.
-             */
-//            System.out.println("WORKSOFARb4");
-//            if (checkStartComp()) {
-//                startComp(setOfParts);
-//            } else {
-//                stopComp(setOfParts);
-//            }
-
-//            System.out.println("WORKSOFARb5");
-//            setOfParts.runHopper();
             runLFDrive(setOfParts);
             runLBDrive(setOfParts);
             runRFDrive(setOfParts);
             runRBDrive(setOfParts);
+
             runDropper(setOfParts);
+
             runShooter1(setOfParts);
             runShooter2(setOfParts);
-//            System.out.println("WORKSOFARb6");
+
             runLauncher(setOfParts);
-//            System.out.println("WORKSOFARb7");
-//            runGyro(setOfParts);
-            ItoggleComp(setOfParts);
-            OtoggleComp(setOfParts);
-            checkStartComp(setOfParts);
+
+            climbMechU(setOfParts);
+            climbMechD(setOfParts);
+            
             Timer.delay(0.05);
         }
     }
--- src/edu/wpi/first/wpilibj/templates/FrisbeeParts.java
+++ src/edu/wpi/first/wpilibj/templates/FrisbeeParts.java
@@ -6,10 +6,9 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStationLCD;
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  *
@@ -24,57 +23,46 @@ public class FrisbeeParts {
     private final static int FShootP = 6;
     private final static int BShootP = 5;
     private final static int DropperP = 7;
-    
-    public Jaguar LFDrive;
-    public Jaguar LBDrive;
-    public Jaguar RFDrive;
-    public Jaguar RBDrive;
-    public Jaguar Shoot1;
-    public Jaguar Shoot2;
-    public Jaguar Dropper;
-    
-//    public Relay Spike;
-    public Solenoid sU, sD;
+    public Talon LFDrive;
+    public Talon LBDrive;
+    public Talon RFDrive;
+    public Talon RBDrive;
+    public Talon ShootFront;
+    public Talon ShootBack;
+    public Talon Dropper;
+    public Solenoid lU, lD, cU1, cU2, cD1, cD2;
     public Compressor airComp;
-        
     public Joystick joy1;
     public Joystick joy2;
-    
-    public boolean IOComp;
-    
     public DriverStationLCD textOutput;
-    
     private static FrisbeeParts partInst;
 
     public FrisbeeParts() {
-        LFDrive = new Jaguar(LFDriveP);
-        LBDrive = new Jaguar(LBDriveP);
-        RFDrive = new Jaguar(RFDriveP);
-        RBDrive = new Jaguar(RBDriveP);
-        Shoot1 = new Jaguar(FShootP);
-        Shoot2 = new Jaguar(BShootP);
-        Dropper = new Jaguar(DropperP);
-        
-        IOComp = false;
-        
+        LFDrive = new Talon(LFDriveP);
+        LBDrive = new Talon(LBDriveP);
+        RFDrive = new Talon(RFDriveP);
+        RBDrive = new Talon(RBDriveP);
+
+
+        ShootFront = new Talon(FShootP);
+        ShootBack = new Talon(BShootP);
+        Dropper = new Talon(DropperP);
+
+
         textOutput = DriverStationLCD.getInstance();
-        
+
         airComp = new Compressor(1, 1);
-//        Spike = new Relay(1, Relay.Direction.kBoth);
-        sU = new Solenoid(1);
-        sD = new Solenoid(2);
-        
+        lD = new Solenoid(1);
+        lU = new Solenoid(2);
+        cU1 = new Solenoid(3);
+        cU2 = new Solenoid(5); //front climb
+        cD1 = new Solenoid(4);
+        cD2 = new Solenoid(6); //front climb
+
         joy1 = new Joystick(1);
         joy2 = new Joystick(2);
     }
-    
-//    public void runHopper(){
-//        textOutput.println(DriverStationLCD.Line.kUser1, 1, "Speed 1 "+ joy1.getAxis(Joystick.AxisType.kY));
-//        textOutput.println(DriverStationLCD.Line.kUser2, 1, "Speed 2 "+ joy2.getAxis(Joystick.AxisType.kY));
-////        textOutput.println(DriverStationLCD.Line.kUser3, 1, "Gyro "+ gyro.getAngle());
-//        textOutput.updateLCD();
-//    }
-//    
+
     public static FrisbeeParts getInstance() {
         if (partInst == null) //if partInst doesnt exist make it!!
         {
